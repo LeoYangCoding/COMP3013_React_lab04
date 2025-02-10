4adaa43e9f3f95619fa5c3de2d8297919b8359fb
LeoYangCoding <leoyangcoding@gmail.com>
2025-02-09 20:40:11 -0800

    Step 1 Create button done
    Create button will be disabled when no content is entered.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 // Tingtao (Leo) Yang
+import React from "react"
 import {useState} from "react"
 import { Header } from "./components/Header";
 import { Assignments } from "./components/Assignments";
 
 
 function App() {
-  const [assignmentList, setAssginment] = useState<object|null>(); 
+  const [assignmentList, setAssignmentList] = useState<string|null>(null); 
   return (
     <>
 
-      <Header />
-      <Assignments />
+      <Header assignmentList={assignmentList} setAssignmentList={setAssignmentList}/>
+      <Assignments assignmentList={assignmentList}/>
     </>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,20 @@
+import {useState} from "react"
 import styles from "./header.module.css";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { uppercase } from "../../helpers/stringHelpers";
 
-export function Header() {
+type HeaderProps = {assignmentList: string|null, setAssignmentList:string|null}
+export function Header(prop:HeaderProps) {
+
+  const [assignmentList, setAssignmentList] = useState<string|null>(null); 
+  
   return (
     <header className={styles.header}>
       {/* This is simply to show you how to use helper functions */}
       <h1>{uppercase("bcit")} Assignment Tracker</h1>
       <form className={styles.newAssignmentForm}>
-        <input placeholder="Add a new assignment" type="text" />
-        <button>
+        <input placeholder="Add a new assignment" type="text" onChange={(e) => prop.setAssignmentList(e.target.value)}/>
+        <button disabled={!prop.assignmentList}  >
           Create <AiOutlinePlusCircle size={20} />
         </button>
       </form>
