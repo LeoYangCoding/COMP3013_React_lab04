fe7e8a81bf318a4237edc624311674464aedca1f
LeoYangCoding <leoyangcoding@gmail.com>
2025-02-09 22:54:25 -0800

    completed the onSubmit event

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,14 +7,25 @@ type HeaderProps = {assignmentList: string[]|null, setAssignmentList:React.Dispa
 export function Header(prop:HeaderProps) {
 // export function Header() {
 
-  const [inputAssignment, setInputAssignment] = useState<string|null>(null); 
+  const [inputAssignment, setInputAssignment] = useState(""); 
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    if(prop.assignmentList){
+      prop.setAssignmentList([...prop.assignmentList,inputAssignment]);
+    }else{
+      prop.setAssignmentList([inputAssignment])
+    }
+    setInputAssignment("");
+  }
 
   return (
     <header className={styles.header}>
       {/* This is simply to show you how to use helper functions */}
       <h1>{uppercase("bcit")} Assignment Tracker</h1>
-      <form className={styles.newAssignmentForm}>
-        <input placeholder="Add a new assignment" type="text" onChange={(e) => setInputAssignment(e.target.value)}/>
+      <form className={styles.newAssignmentForm} onSubmit = {handleSubmit}>
+        <input placeholder="Add a new assignment" type="text" value={inputAssignment} onChange={(e) => setInputAssignment(e.target.value)}/>
         <button disabled={!inputAssignment}  >
           Create <AiOutlinePlusCircle size={20} />
         </button>
@@ -23,6 +34,4 @@ export function Header(prop:HeaderProps) {
   );
 }
 
-// function addToList(setAssignmentList,assignmentList){
 
-// }
